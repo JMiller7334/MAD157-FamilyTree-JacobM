01167f11bbc9256e919e1bd3094c52f38aca4491
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2022-02-20 15:31:07 -0600

    Initial Commit

---
+++ MAD157-FamilyTreeApp-JacobM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD157-FamilyTreeApp-JacobM
+//
+//  Created by student on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
